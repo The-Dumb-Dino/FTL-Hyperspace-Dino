a4b46698045bb3b5a9e2febdb9b87362e91bce56
fr-eed <freed@duck.com>
2025-12-06 02:24:20 +0100

    Update vcpkg dependencies and CI devcontainer fixes
    Removed boost-filesystem and added boost-atomic to vcpkg.json due to a linking issue with boost-atomic's library naming in vcpkg. Updated devcontainer-fixes.sh with comments explaining the workaround for the boost-atomic .lib naming issue and clarified the vcpkg update step.

--- buildscripts/ci/devcontainer-fixes.sh
+++ buildscripts/ci/devcontainer-fixes.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-# Update vcpkg to get latest package versions
+# Update vcpkg to get latest package versions to avoid issues with old cached versions
+# Note: remove when new devcontainer image will be built
 (cd /vcpkg && git pull && ./bootstrap-vcpkg.sh)
+
+
+# This script applies fixes to a bug caused by a x86-windows-ftl.cmake triplet or/and toolchain.
+# For some reason only boost_atomic library is being built with .lib naming instead of .a like all other boost libs.
+# This causes linking errors when boost-filesystem (which depends on boost-atomic) is used in a MinGW build.
+# As a workaround, we manually copy boost_atomic.lib to libboost
 
 # Try to install all packages to both Windows build directories
 # boost-filesystem will fail due to boost-atomic lib naming, that's expected
